3de6da3a5dce136e2b799beca2ef97c262b17044
Matheus Arakaki <arakakimath@gmail.com>
2025-02-18 11:05:41 -0400

    start dealing with modules, s3 first and deleting files in root folder

---
+++ README.md
@@ -0,0 +1,18 @@
+# Primeiro projeto de IAC (Infrastructure As Code)
+
+Neste primeiro projeto, foi usado terraform e recursos aws.
+
+## To configure aws sso
+```
+aws configure sso
+```
+
+## To login
+```
+aws sso login --profile profile_name
+```
+
+## To initialize terraform
+```
+terraform init
+```
--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "${var.user_name}-bucket-iac-${terraform.workspace}"
+module "s3" {
+  source         = "./modules/s3"
+  s3_bucket_name = "arakakimath-iac"
+}
 
-  tags = {
-    Name = "Primeiro bucket"
-    Iac = true
-    context = "${terraform.workspace}"
-  }
+module "cloudfront" {
+  source             = "./modules/cloudfront"
+  origin_id          = module.s3.bucket_id
+  bucket_domain_name = module.s3.bucket_domain_name
+
+  depends_on = [module.s3]
 }
